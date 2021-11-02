b326693261e4a530a0f291f606fecbb3a796b31e
TanishiDesai <72441998+TanishiDesai@users.noreply.github.com>
2021-11-02 13:08:07 +0530

    Add files via upload

---
+++ intro.py
@@ -0,0 +1,8 @@
+name = input("Enter your Name : ")
+age = input("Enter your age: ")
+profession = input("Enter your profession : ")
+hobby = input("Enter your hobbies : ")
+aim = input("Enter your aim : ")
+
+print("Hello! Could you please introduce yourself?")
+print("Greetings! I don't think we have met before, I'm " + name + "." + "I am " + age  + " years old. " + "My hobbies are " + hobby + "." + "My aim is to become a " + aim + ".")
